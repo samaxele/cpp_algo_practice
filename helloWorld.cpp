91be256750c99818fd085cc31f848e9e07b6342b
Sam <91824257+heteroyeen@users.noreply.github.com>
2022-01-21 00:17:24 +0800

    test commit message

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 #include "myCustomHeader.h" 
+#include <ctime>
 
 /*
 First Ubuntu code!
 */
 
 int main() {
-    std::vector<int> testcase = {2,3,5,3,2};
-    std::cout<< "Hello World\n";
-    std::cout << "My vector is ";
-    printList(testcase);
-    std::cout << std::endl;
+    /*
+    Init the time
+    time_t time(time_t *time);
+    >> return current calendar time since 1970 01 01
+    We will have 'now' storing our time data.
+    We are creating an object 'now', not pointer
+    */
+    time_t now = time(0);
+
+    /*
+    char *ctime(const time_t *time);
+    This will convert any time data from time_t to a human-readable string
+    the function is ctime.
+    Since we have now as data, we need to pass by reference -> &now.
+    */
+    std::string myLocalTime = ctime(&now);
+
+    std::cout<< "Build date = " << myLocalTime;
+    std::cout << "\n";
 
     return 0;
 }
